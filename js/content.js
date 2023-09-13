da5fc6ad00562e97f1ab3e76691967fbeb32c44e
sengu30 <dearseng@gmail.com>
2023-09-14 03:18:32 +0900

    edit node

--- js/content.js
+++ js/content.js
@@ -1,21 +1,35 @@
+const quotesvg =document.createElement("div");
+quotesvg.innerHTML=`
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                </svg>`;
 
 
-setInterval(
-    function () {
-        const articles = document.querySelectorAll("article");
-        console.log(articles.length);
-        articles.forEach((e) => {
-            let linklist = e.querySelectorAll("a")
-            //console.log(linklist[3].getAttribute('href'));
-            //console.log(linklist[4].getAttribute('href'));
-            linklist.forEach(
-                function (f, index) {
-                    if (f.querySelector("time")) {
-                        console.log(f.getAttribute('href'));
-                    }
+const addQuoteBtn = function () {
+    const articles = document.querySelectorAll("article");
+    console.log(articles.length);
+    articles.forEach((e) => {
+        let linklist = e.querySelectorAll("a")
+        linklist.forEach(
+            function (f, index) {
+                if (f.querySelector("time")) {
+                    console.log(f.getAttribute('href'));
                 }
-            )
-        })
-
-    }, 10000
-)
+            }
+        )
+        //버튼 추가
+        let group = e.querySelectorAll(`[role="group"] > div`)
+        const rtbtn=group[1];
+        const svgpa=rtbtn.querySelector('svg').parentNode;       
+        //console.log(svgpa)
+        const clonesvg=rtbtn.querySelector('svg').cloneNode(true);
+        console.log(quotesvg.childNodes)
+        console.log(clonesvg)
+        rtbtn.insertAdjacentHTML("afterend",clonesvg);
+        svgpa.replaceChild(quotesvg.childNodes[1],clonesvg);//원래 있던게 교체되어서 다른 방법이 필요함
+      
+    })
+}
+setInterval(addQuoteBtn, 5000);
--- js/quotebtn.js
+++ js/quotebtn.js
@@ -1,3 +1,4 @@
+const quotebtn = `
 <div class="css-1dbjc4n r-18u37iz r-1h0z5md">
     <div aria-disabled="true" aria-expanded="false" aria-haspopup="menu" aria-label="0 재게시. 재게시" role="button"
         tabindex="-1" class="css-1dbjc4n r-1777fci r-bt1l66 r-icoktb r-1ny4l3l r-bztko3 r-lrvibr" data-testid="retweet">
@@ -7,9 +8,10 @@
                 <div
                     class="css-1dbjc4n r-1niwhzg r-1p0dtai r-xoduu5 r-1d2f490 r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg">
                 </div>
-                인용보기
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
-                    <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
                 </svg>
             </div>
             <div class="css-1dbjc4n r-xoduu5 r-1udh08x"><span data-testid="app-text-transition-container"
@@ -18,4 +20,4 @@
             </div>
         </div>
     </div>
-</div>
+</div>`
